8a9f2b485a198df477a6efdd0016b074bde32427
Jor Daniel <becariosinntec3@gmail.com>
2021-11-13 17:35:49 -0600

    Segundo commit

--- CursoXamarin/CursoXamarin/ViewModels/BooksViewModel.cs
+++ CursoXamarin/CursoXamarin/ViewModels/BooksViewModel.cs
@@ -11,7 +11,11 @@ namespace CursoXamarin.ViewModels {
 		public BooksViewModel() {
 			Books = new ObservableCollection<Book>() {
 				new Book("Los 4 acuerdos","Miguel ruíz",298,198.50f,true),
-				new Book("El sol se muere ","Jamake Highwater",378,177.50f,false)
+				new Book("El sol se muere ","Jamake Highwater",378,177.50f,false),
+				new Book("Antología ","Verónica Hernández",511,342.37f,true),
+				new Book("Don quijote de la mancha","Miguel de Cervantes Saavedra",772,512.34f,false),
+				new Book("Número cuatro","William Sort", 488, 210.9f, true), 
+				new Book("Portal","Claudia Figueroa", 212, 543.98f, false)
 			};
 		}
 	}
